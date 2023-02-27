03047386977a8d05de6d1bfa7a13d02998006039
Ludobico <aqs450@gmail.com>
2023-02-27 15:51:42 +0900

    add CSStransition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Main from "./Components/Main.jsx";
+import Test from "./Components/Test";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <TransitionGroup className="transition-group">
+        <CSSTransition timeout={1000} classNames="fade">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
---
+++ src/Components/Test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div style={{ background: "white", width: "100vw", height: "100vh" }}></div>
+  );
+};
+
+export default Test;
